b092b16734903d1b43dc6894c9500360ba4e4df7
Madiar <madiar.bakbergenov10@hmail.com>
2023-01-05 02:33:23 +0600

    Add library ServiceContracts.csproj and CreateProjectCommand.cs command, ProjectVm.cs ViewModel

---
+++ ServiceContracts/Project/Commands/CreateProjectCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace ServiceContracts.Project.Commands;
+
+public class CreateProjectCommand
+{
+    
+}
---
+++ ServiceContracts/Project/Models/ProjectVm.cs
@@ -0,0 +1,10 @@
+﻿using ProjectManager.Domain.Complementary;
+
+namespace ServiceContracts.Project.Models;
+
+public class ProjectVm
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public StateEnums State { get; set; }
+}
