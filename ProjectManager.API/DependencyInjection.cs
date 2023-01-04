9ae79ed764de526bf3472a335723736ba602c1d7
Madiar <madiar.bakbergenov10@hmail.com>
2023-01-05 03:59:20 +0600

    rename swagger title

--- ProjectManager.API/DependencyInjection.cs
+++ ProjectManager.API/DependencyInjection.cs
@@ -22,7 +22,7 @@ public static class DependencyInjection
     {
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineWallet.API", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectManager.API", Version = "v1" });
             c.EnableAnnotations();
         });
         return services;
